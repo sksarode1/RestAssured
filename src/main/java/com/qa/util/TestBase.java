0c5f6b2fa71f6f8d3fd5b3a6777538dddd85f053
Sagar Sarode <sksarode1@gmail.com>
2020-09-19 23:30:12 +0530

    first commit

---
+++ src/main/java/com/qa/util/TestBase.java
@@ -0,0 +1,33 @@
+package com.qa.util;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+public class TestBase {
+	public static Properties prop;
+	
+	public static void init(){
+		prop = new Properties();
+		try {
+			FileInputStream ip = new FileInputStream("C:\\Users\\Sagar\\Downloads\\RestAssuredFramework-master\\"
+		 	+ "src/main/java/com/qa/config/config.properties");
+			prop.load(ip);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+
+}
